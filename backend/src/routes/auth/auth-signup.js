26bbd9aa9bcb5f7ea94ae9938171e0dcdecf1ddd
BheshRajNeupane <108607897+BheshRajNeupane@users.noreply.github.com>
2024-06-09 11:34:33 +0545

    user data save with hash password

--- backend/src/routes/auth/auth-signup.js
+++ backend/src/routes/auth/auth-signup.js
@@ -53,28 +53,6 @@ catch(err){
     }
 
  
-
-  //  const user = User.build({ email, password });
-  //  console.log(user);
-  //  await user.save();
-
-  //  // Generate JWT
-  //  const userJwt = jwt.sign(
-  //    {
-  //      id: user.id,
-  //      email: user.email,
-  //    },
-  //    "asdf"
-  //    // process.env.JWT_KEY!
-  //  );
-
-  //  // Store it on session object
-  //  req.session = {
-  //    jwt: userJwt,
-  //  };
-
-//    res.status(201).send(user);
-//  }
  )
     
 
